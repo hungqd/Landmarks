d6cc415386fa52bf3c2b1b8ba3160cb42365dc73
Đặng Quang Hưng <hungdq58@gmail.com>
2019-09-26 20:19:26 +0700

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Đặng Quang Hưng on 9/26/19.
+//  Copyright © 2019 Đặng Quang Hưng. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

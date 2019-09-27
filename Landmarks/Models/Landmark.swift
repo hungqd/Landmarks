8cc17e4562fb2b07f0e4bc17b2e05f93a059856c
Đặng Quang Hưng <hungdq58@gmail.com>
2019-09-27 21:39:09 +0700

    Building Lists and Navigation

---
+++ Landmarks/Models/Landmark.swift
@@ -0,0 +1,43 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Đặng Quang Hưng on 9/27/19.
+//  Copyright © 2019 Đặng Quang Hưng. All rights reserved.
+//
+
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    fileprivate var imageName: String
+    fileprivate var coordinates: Coordinates
+    var state: String
+    var park: String
+    var category: Category
+
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+
+    enum Category: String, CaseIterable, Codable, Hashable {
+        case featured = "Featured"
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+    }
+}
+
+extension Landmark {
+    var image: Image {
+        ImageStore.shared.image(name: imageName)
+    }
+}
+
+struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
+}
--- Landmarks/Supporting Views/MapView.swift
+++ Landmarks/Supporting Views/MapView.swift
@@ -10,12 +10,13 @@ import SwiftUI
 import MapKit
 
 struct MapView: UIViewRepresentable {
+    var coordinate: CLLocationCoordinate2D
+
     func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
         MKMapView(frame: .zero)
     }
 
     func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
-        let coordinate = CLLocationCoordinate2D(latitude: 16.0676787, longitude: 108.19352)
         let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
         let region = MKCoordinateRegion(center: coordinate, span: span)
         uiView.setRegion(region, animated: true)
@@ -24,6 +25,6 @@ struct MapView: UIViewRepresentable {
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(coordinate: landmarkData[0].locationCoordinate)
     }
 }
